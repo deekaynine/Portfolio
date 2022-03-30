b7a6a72b0f0bb903fcad0bda8c5d5bd3eca47ecc
unknown <deekaynine@gmail.com>
2022-03-30 12:29:31 -0400

    fixed imports

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import HTML from "../assets/html.png";
-import CSS from "../assets/css.png";
 import JavaScript from "../assets/javascript.png";
 import ReactImg from "../assets/react.png";
 import Node from "../assets/node.png";
